343b368774e966b07c9c473f8a3ca60a390e1957
Santiago Botina <santiagoboe04@gmail.com>
2026-08-06 19:06:05 -0500

    refactor: fix navbar

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,15 @@
 class LandingController < ApplicationController
-  before_action :require_no_authentication, only: [ :index ]
+  before_action :redirect_signed_in_users, only: [ :index ]
 
   def index
+  end
+
+  private
+
+  # Redirect signed-in users away from the marketing page without a flash.
+  # The "already logged in" notice is reserved for the login/registration flows
+  # (see require_no_authentication in ApplicationController).
+  def redirect_signed_in_users
+    redirect_to after_sign_in_path if signed_in?
   end
 end
